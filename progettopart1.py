1e02826b2821fb1e76a6f6639ac0265d30ee1be8
cristina2300 <112753206+cristina2300@users.noreply.github.com>
2022-09-12 15:35:48 +0200

    Update progettopart1.py

--- progettopart1.py
+++ progettopart1.py
@@ -5,39 +5,38 @@ from progettopart2 import *
 
 class Registry:
     def __init__(self):
-        self.__datad = pd.read_csv('disease_evidences.tsv', sep= '\t')
-        self.__datag = pd.read_csv('gene_evidences.tsv', sep = '\t')
+        self.__datad = pd.read_csv('disease_evidences.tsv.gz', sep= '\t')
+        self.__datag = pd.read_csv('gene_evidences.tsv.gz', sep = '\t')
         self.__operations = ['Metadata', 'Semantics', 'Genes', 'Diseases', 'List of sentences of genes related to COVID-19',
                             'List of sentences of diseases related to COVID-19', 'Top 10 associations',
                             'Disease list related to a gene symbol or ID', 'Gene list related to a disease name or ID']
         self.__hyperlinks  = ['Meta', 'Sem', 'Genes', 'Diseases', 'inputg', 'SG', 'inputd', 'SD', 'Top', 'GetoDis', 'DistoGe' ]
         
-        
-        
-        def registry(self):
-            return self.__operations
 
-        def links(self):
-            return self.__hyperlinks
-        def metadata(self): 
-            return DataCollection(self.__datad, self.__datag).shape()
-        def semantics(self): #2
-            return DataCollection(self.__datad, self.__datag).get_label()
-        def genes(self): #3
-            return Detection(self.__datag).genesymbol_detect()
-        def inputg(self):
-            return Inputg(self.__datag).list_genes()
-        def inputd(self):
-            return Inputd(self.__datad).list_diseases()
-        def sentenceg(self): #4 
-            return Sentence(self.__datag).find_sentenceg()
-        def diseases(self): #5
-            return Detection(self.__datad).diseasename_detect()
-        def sentenced(self): #6 
-            return Sentence(self.__datad).find_sentenced()
-        def top10(self): #7
-            return TopTen(self.__datad, self.__datag).top_ten()
-        def genetod(self): #8 
-            return AssociationList(self.__datag, self.__datad).association()
-        def diseasetog(self): #9 
-            return AssociationList(self.__datag, self.__datad).association()
+    def registry(self):
+        return self.__operations
+
+    def links(self):
+        return self.__hyperlinks
+    def metadata(self): 
+        return DataCollection(self.__datad, self.__datag).shape()
+    def semantics(self): #2
+        return DataCollection(self.__datad, self.__datag).get_label()
+    def genes(self): #3
+        return Detection(self.__datag).genesymbol_detect()
+    def inputg(self):
+        return Inputg(self.__datag).list_genes()
+    def inputd(self):
+        return Inputd(self.__datad).list_diseases()
+    def sentenceg(self, n): #4 
+        return Sentence(self.__datag).find_sentenceg()
+    def diseases(self): #5
+        return Detection(self.__datad).diseasename_detect()
+    def sentenced(self): #6 
+        return Sentence(self.__datad).find_sentenced()
+    def top10(self): #7
+        return TopTen(self.__datad, self.__datag).top_ten()
+    def genetod(self): #8 
+        return AssociationList(self.__datag, self.__datad).association()
+    def diseasetog(self): #9 
+        return AssociationList(self.__datag, self.__datad).association()
